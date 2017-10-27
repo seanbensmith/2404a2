95be0edca0460e2d59b368c1c1440509dfc38531
Michael Marcuzzi <32044154+MichaelMarcuzzi@users.noreply.github.com>
2017-10-27 16:38:43 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                   */
+/*  Program:  MyTunes Music Player                   */
+/*  Author:   Louis Nel                              */
+/*  Date:     21-SEP-2017                            */
+/*                                                   */
+/*  (c) 2017 Louis Nel                               */
+/*  All rights reserved.  Distribution and           */
+/*  reposting, in part or in whole, requires         */
+/*  written consent of the author.                   */
+/*                                                   */
+/*  COMP 2404 students may reuse this content for    */
+/*  their course assignments without seeking consent */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "song.h"
+#include "songs.h"
+
+int main()
+{
+  cout << "Hello World" << endl;
+  //Put some code here to test Song and Songs
+
+  return 0;
+}
+
