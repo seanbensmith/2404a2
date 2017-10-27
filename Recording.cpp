7706b6cc53269c9915cea9d8694943eb824ecd73
seanbensmith <32254421+seanbensmith@users.noreply.github.com>
2017-10-27 15:38:40 -0400

    Create Recording.cpp

---
+++ Recording.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "recording.h"
+
+Recording::Recording(const string & aTitle, const string & anArtist, const int  aYear, const string & aProducer, const int anID){
+	cout << "Recording(string&, string&, int, string&)" << this << endl;
+	title = aTitle;
+	artist = anArtist;
+	year = aYear;
+	producer = aProducer;
+	id = anID;
+}
+Recodring::~Recording(){
+	cout << "~Song(void)" << this << endl;
+}
+int Recording::getID(){return id;}
+
+string Recording::toString()const{
+	return to_string(id) + " " + title + " " artist + " " + year + " " + producer;
+}
+
+ostream & operator<<(ostream & out, const Recording & aRecording){
+	out << aRecording.toString() << endl;
+	return out;
+}
