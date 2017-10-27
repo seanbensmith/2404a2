d57ffdaf14f703b5d5633a4f4c20a77c22368f2c
Michael Marcuzzi <32044154+MichaelMarcuzzi@users.noreply.github.com>
2017-10-27 16:01:57 -0400

    Create playlist.h

---
+++ playlist.h
@@ -0,0 +1,24 @@
+#ifndef _PLAYLIST_H
+#define _PLAYLIST_H
+
+#include <ostream>
+#include <string>
+
+class Playlist {
+	/*
+	Playlist represents a grouping of songs
+	*/
+	public:
+	Playlist(const string & aName, const int anId = -1);
+	~Playlist(void);
+    	int getID();
+    	string toString() const;
+	
+	private:
+	string title;
+	int id;
+};
+
+ostream & operator<<(ostream & out, const Song & aPlaylist);
+
+#endif
