6d26f64553669cf2e45050685c15f66a7dd4b056
seanbensmith <32254421+seanbensmith@users.noreply.github.com>
2017-10-27 16:21:44 -0400

    Create tracks.cpp

---
+++ tracks.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#include "tracks.h"
+#include "track.h"
+	
+Tracks::Tracks(){
+}
+Tracks::~Tracks(void){
+	for(vector<Track*>::size_type i=0; i<collection.size(); i++)
+		delete collection[i]; //delete songs in this container
+}
+vector<Track*>::iterator Tracks::findPosition(Track & aTrack){
+	for (vector<Track*>::iterator it = collection.begin() ; it != collection.end(); ++it)
+		if(*it == &aTrack) return it;
+	return collection.end();
+}
+Track * Tracks::findByID(int anID){
+	for (vector<Track*>::iterator it = collection.begin() ; it != collection.end(); ++it)
+		if((*it)->getID() == anID) return *it;
+	return NULL;
+}
+void Tracks::add(Track & aTrack){
+	collection.push_back(&aTrack);
+}
+void Tracks::remove(Track & aTrack){
+	vector<Track*>::iterator index = findPosition(aTrack);
+	if(index != collection.end()) {
+		Track * theTrack = *index;
+		collection.erase(index);
+		delete theTrack; //free the memory of theSong
+	}
+}
+/*
+void Songs::showOn(UI & view) const {
+  view.printOutput("SONGS:");
+  for(int i=0; i<collection.size(); i++)
+       view.printOutput((*collection[i]).toString());	  
+}
+*/
+
+void Tracks::printOn(ostream & out) const {
+  cout << "TRACKS:" << endl;
+  for(vector<Track*>::size_type i=0; i<collection.size(); i++)
+       cout << (*collection[i]).toString() << endl;	  
+}
+
+ostream & operator<<(ostream & out, const Songs & songs){
+  songs.printOn(out);
+  return out;
+}
