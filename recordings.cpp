e8313ad0c1d23725a3b2d05cba2c1642210f48eb
seanbensmith <32254421+seanbensmith@users.noreply.github.com>
2017-10-27 15:59:33 -0400

    Create recordings.cpp

---
+++ recordings.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#include "songs.h"
+#include "song.h"
+
+Recordings::Recordings(){
+}
+Recordings::~Recordings(void){
+	for(vector<Recording*>::size_type i=0; i<collection.size(); i++)
+		delete collection[i];
+}
+vector<Recording*>::iterator Recordings::findPosition(Recording & aRecording){
+	for(vector<Recording*>::iterator it = collection.begin() ; it != collection.end(); ++it)
+		if(*it == &aRecording) return it;
+	return collection.end();
+}
+Recording * Recordings::findByID(int anID){
+	for (vector<Recording*>::iterator it = collection.begin() ; it != collection.end(); ++it)
+		if((*it)->getID() == anID) return *it;
+	return NULL;
+}
+void Recordings::add(Recording & aRecording){
+	collection.push_back(&aRecording);
+}
+void Recordings::remove(Recording & aRecording){
+	vector<Recording*>::iterator index = findPosition(aRecording);
+	if(index != collection.end()){
+		Recording * theRecording = *index;
+		collection.erase(index);
+		delete theRecording;
+	}
+}
+
+void Recordings::printOn(ostream & out) const {
+	cout << "Recordings:" << endl;
+	for(vector<Recording*>::size_type i=0; i<collection.size(); i++)
+		cout << (*collection[i]).toString() << endl;
+}
+
+ostream & operator<<(ostream & out, const Recordings & recordings){
+	recordings.printOn(out);
+	return out;
+}
