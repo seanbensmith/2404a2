436faf78f0718f9fd2890c2a678c84d1633c8345
Michael Marcuzzi <32044154+MichaelMarcuzzi@users.noreply.github.com>
2017-10-27 16:03:39 -0400

    Create playlists.h

---
+++ playlists.h
@@ -0,0 +1,28 @@
+#ifndef _PLAYLISTS_H
+#define _PLAYLISTS_H
+
+#include <ostream>
+#include <vector>
+#include <string>
+using namespace std;
+//#include "UI.h"
+#include "playlist.h"
+
+class Playlists {
+  
+	public:
+	Playlists(); 
+	~Playlists(void);
+	Playlists * findByID(int anID);
+	void add(Playlist & aPlaylist);
+	void remove(Playlist & aPlaylist);
+	//void showOn(UI & aView) const;
+	void printOn(ostream & out) const;
+	
+	private:
+	vector<Playlist*> collection;
+	vector<Playlist*>::iterator findPosition(Playlist & aPlaylist);
+};
+
+ostream & operator<<(ostream & out, const Playlists & playlists);
+#endif
