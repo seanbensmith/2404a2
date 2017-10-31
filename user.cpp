fba6e9b7529e00846d03e2970fabed296f05ef3c
seanbensmith <32254421+seanbensmith@users.noreply.github.com>
2017-10-31 12:51:05 -0400

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 #include "user.h"
 	
-User::User(const int aUserID, const string & aName){
+User::User(const int aUserID , const string & aName){
 	cout << "User(int, string&) " << this << endl;
 	userID = aUserID;
 	name = aName;
@@ -12,13 +12,16 @@ User::User(const int aUserID, const string & aName){
 User::~User(){
 	cout << "~User(void) "  << this << endl;
 }
-int User::getID(){return id;}
+int User::getID(){return userID;}
+//string User::getName(){return name;}
+//vector<Playlists*>		how to make playlists a collection of user
 
-string Playlist::toString()const {
-	return to_string(id) + " " + userID + " " + name ".";
+string User::toString()const {
+	std::string IDstr = std::to_string(userID);
+	return IDstr + name + ".";
 }
 
-ostream & operator<<(ostream & out, const USer & aUser){
+ostream & operator<<(ostream & out, const User & aUser){
 	out << aUser.toString() << endl;
 	return out;
 }
