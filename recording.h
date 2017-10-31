069b92cc0d2026f7bc68b2629ae01f661ff48fdf
Michael Marcuzzi <32044154+MichaelMarcuzzi@users.noreply.github.com>
2017-10-31 12:14:37 -0400

    Update recording.h

--- recording.h
+++ recording.h
@@ -7,14 +7,19 @@
 class Recording {
 
 	public:
-	Recording(const string & aTitle, const string & anArtist const int aYear, const string & aProducer, const int anID = -1);
+	Recording(const string & aTitle, const string & anArtist, const int aYear, const string & aProducer, const int anID = -1);
 	~Recording(void);
 	int getID();
+	//int getYear();
+	//string getTitle();
+	//string getArtist();
+	//string getProducer();
 	string toString() const;
 	
 	private:
 	string title;
 	string artist;
+	string producer;
 	int year;
 	int id;
 };
